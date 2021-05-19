06608317b80f93e41cd3bf4eaf35ee09fd7caab1
melvindionisio <melvinnudo.dionisio021@gmail.com>
2021-05-19 09:34:48 +0800

    Done

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const countedText = document.querySelector('.counted-text');
 const indicator = document.querySelector('.indicator');
 const toggleInclude = document.querySelector('.include-input');
 
+const setCounted = () => {
+    let textTotalCount = Array.from(textOrigin.value);
+    if (!toggleInclude.checked) {
+        textTotalCount = textTotalCount.filter(char => char !== " ");
+        countedText.innerText = textTotalCount.length;
+    } else {
+        countedText.innerText = textTotalCount.length;
+    }
+}
 
-textOrigin.addEventListener('keyup', ()=>{
-	let textTotalCount = Array.from(textOrigin.value);
-
-	if(!toggleInclude.checked){
-		textTotalCount = textTotalCount.filter(char=>char !== " ");
-		countedText.innerText = textTotalCount.length;
-	} else {
-		countedText.innerText = textTotalCount.length;
-	}
+textOrigin.addEventListener('keyup', () => {
+    setCounted();
 });
-toggleInclude.addEventListener('change', ()=>{
-	toggleInclude.checked ? indicator.textContent = "(spaces included)" : indicator.textContent = "(spaces not included)";
+
+toggleInclude.addEventListener('change', () => {
+    toggleInclude.checked ? indicator.textContent = "(spaces included)" :
+        indicator.textContent = "(spaces not included)";
+
+    setCounted();
 })
-
-
